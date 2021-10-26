ce8d467979e231000afbb49e61f980b4f5588ab2
ericwachiramwaniki <91129232+ericwachiramwaniki@users.noreply.github.com>
2021-10-26 16:12:10 +0300

    Add files via upload

---
+++ quadratic eric.c
@@ -0,0 +1,44 @@
+/*
+  project:Quadratic Equation Solution
+  Author:Eric Wachira
+  Date:26th Oct,2021
+  Compiler:GCC
+  Language:C99
+  License:MIT
+
+INPUTS->a,b,c;
+OUTPUTS-> x1,x2;
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{   //variable declaration
+    int a,b,c;
+    float x1,x2;
+
+    //Capture Input
+    printf("\tQuadratic Equation Solver!\n");
+    printf("Enter the values a,b and c: ");
+    scanf("%d%d%d",&a,&b,&c);
+
+    //processing/computation
+    if((b*b-4*a*c)==0) {
+            x1 = x2 = (-b+ sqrt(b*b-4*a*c))/(2*a);
+    }
+    if((b*b-4*a*c) > 0) {
+       x1  = (-b+ sqrt(b*b-4*a*c))/(2*a);
+       x2 = (-b+ sqrt(b*b-4*a*c))/(2*a);
+    }
+    if((b*b-4*a*c) < 0) {
+       printf("The equation has no roots\n");
+       exit(1);
+    }
+
+//Outputs
+printf("The values of x are %f and %f\n",x1,x2);
+return 0;
+
+}
+
